ce07b3da3d18840b19ec36d64d9d90505b66ed39
t-march <117422344+t-march@users.noreply.github.com>
2023-07-14 19:29:12 -0300

    .

---
+++ mod18/Anotation/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+    @Tabela("Nome da Tabela")
+    public static void teste(){
+        System.out.println("Teste");
+    }
+}
